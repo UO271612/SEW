5744ab199414fd4d919196ca58861c9d4418068b
UO271612 <UO271612@uniovi.es>
2020-11-15 00:41:10 +0100

    Ejercicio 2

---
+++ MasInfoNavegador.js
@@ -0,0 +1,20 @@
+document.write("<p lang=\"en\">");
+document.write("Versión: ")
+document.write(infoNavegador.version);
+document.write("</p>");
+document.write("<p lang=\"en\">");
+document.write("Plataforma: ")
+document.write(infoNavegador.plataforma);
+document.write("</p>");
+document.write("<p lang=\"en\">");
+document.write("Vendedor: ")
+document.write(infoNavegador.vendedor);
+document.write("</p>");
+document.write("<p lang=\"en\">");
+document.write("Agente: ")
+document.write(infoNavegador.agente);
+document.write("</p>");
+document.write("<p lang=\"en\">");
+document.write("Java activo: ")
+document.write(infoNavegador.javaActivo);
+document.write("</p>");
